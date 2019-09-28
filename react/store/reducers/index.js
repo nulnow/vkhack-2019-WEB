ad6b305f71517654364499b5f11a8684f97f1c96
Andrey Razuvaev <razuvaev@cmsplanet.ru>
2019-09-29 00:26:04 +0300

    fix админки

--- react/store/reducers/index.js
+++ react/store/reducers/index.js
@@ -3,10 +3,12 @@ import eventsReducer from './eventsReducer'
 import profileReducer from './profileReducer'
 import notificationsReducer from './notificationsReducer'
 import museumsReducer from './museumsReducer'
+import usersReducer from './usersReducer'
 
 export  default combineReducers({
     events: eventsReducer,
     profile: profileReducer,
     notifications: notificationsReducer,
     museums: museumsReducer,
+    users: usersReducer,
 })
