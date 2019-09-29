4c4c3ff57573ba85148277daf9104b04ff01664c
Andrey Razuvaev <razuvaev@cmsplanet.ru>
2019-09-29 05:29:57 +0300

    Фронтенд редактирования профиля

--- react/components/AdminPanel/AdminUsers.js
+++ react/components/AdminPanel/AdminUsers.js
@@ -15,9 +15,12 @@ const getEmptyUser = () => ({
 const AdminUsers = ({ isLoading, users, error }) => {
 
     const [filter, setFilter] = useState(null)
+
     const [isAddUserModalOpen, setAddUserModalOpen] = useState(false)
+    const [isEditUserModalOpen, setEditUserModalOpen] = useState(false)
 
     const [newUser, setNewUser] = useState(getEmptyUser())
+    const [editUser, setEditUser] = useState({})
 
     const updateNewUser = (changes) => {
         setNewUser({
@@ -28,6 +31,19 @@ const AdminUsers = ({ isLoading, users, error }) => {
 
     const updateNewUserField = field => e => {
         updateNewUser({
+            [field]: e.target.value
+        })
+    }
+
+    const updateEditUser = (changes) => {
+        setEditUser({
+            ...editUser,
+            ...changes,
+        })
+    }
+
+    const updateEditUserField = field => e => {
+        updateEditUser({
             [field]: e.target.value
         })
     }
@@ -46,12 +62,28 @@ const AdminUsers = ({ isLoading, users, error }) => {
         setNewUser(getEmptyUser())
     }
 
+    const updateUser = () => {
+
+    }
+
     const openAddUserModal = () => {
         setAddUserModalOpen(true)
     }
     const closeAddUserModal = (e) => {
         if (e.target.classList.contains('pop-up')) {
             setAddUserModalOpen(false)
+        }
+    }
+
+    const openEditUserModal = (user) => {
+        setEditUser(user)
+        setEditUserModalOpen(true)
+    }
+
+    const closeEditUserModal = (e) => {
+        if (e.target.classList.contains('pop-up')) {
+            setEditUser({})
+            setEditUserModalOpen(false)
         }
     }
 
@@ -62,20 +94,35 @@ const AdminUsers = ({ isLoading, users, error }) => {
         : users
 
     return <React.Fragment>
-            {
-                isAddUserModalOpen && <div className="pop-up" onClick={closeAddUserModal}>
-                    <div className="send-message">
-                        <h3>Регистрация волонтёра</h3>
-                        <input type="text" value={newUser.firstName} onChange={updateNewUserField('firstName')} className="input-txt" placeholder="Имя"/>
-                        <input type="text" value={newUser.lastName} onChange={updateNewUserField('lastName')} className="input-txt" placeholder="Фамилия"/>
-                        <input type="text"  value={newUser.passport} onChange={updateNewUserField('passport')}  className="input-txt" placeholder="Паспорт"/>
-                        <input type="e-mail" value={newUser.email} onChange={updateNewUserField('email')} className="input-txt" placeholder="E-mail"/>
-                        <input type="password" value={newUser.password} onChange={updateNewUserField('password')} className="input-txt" placeholder="Пароль"/>
-                        <input type="text" value={newUser.avatar} onChange={updateNewUserField('avatar')}  className="input-txt" placeholder="Url фото"/>
-                        <button onClick={registerUser} className="btn">Отправить</button>
-                    </div>
+        {
+            isAddUserModalOpen && <div className="pop-up" onClick={closeAddUserModal}>
+                <div className="send-message">
+                    <h3>Регистрация волонтёра</h3>
+                    <input type="text" value={newUser.firstName} onChange={updateNewUserField('firstName')} className="input-txt" placeholder="Имя"/>
+                    <input type="text" value={newUser.lastName} onChange={updateNewUserField('lastName')} className="input-txt" placeholder="Фамилия"/>
+                    <input type="text"  value={newUser.passport} onChange={updateNewUserField('passport')}  className="input-txt" placeholder="Паспорт"/>
+                    <input type="e-mail" value={newUser.email} onChange={updateNewUserField('email')} className="input-txt" placeholder="E-mail"/>
+                    <input type="password" value={newUser.password} onChange={updateNewUserField('password')} className="input-txt" placeholder="Пароль"/>
+                    <input type="text" value={newUser.avatar} onChange={updateNewUserField('avatar')}  className="input-txt" placeholder="Url фото"/>
+                    <button onClick={registerUser} className="btn">Отправить</button>
                 </div>
-            }
+            </div>
+        }
+
+        {
+            isEditUserModalOpen && <div className="pop-up" onClick={closeEditUserModal}>
+                <div className="send-message">
+                    <h3>Редактирование профиля волонтёра</h3>
+                    <input type="text" value={editUser.firstName} onChange={updateEditUserField('firstName')} className="input-txt" placeholder="Имя"/>
+                    <input type="text" value={editUser.lastName} onChange={updateEditUserField('lastName')} className="input-txt" placeholder="Фамилия"/>
+                    <input type="text"  value={editUser.passport} onChange={updateEditUserField('passport')}  className="input-txt" placeholder="Паспорт"/>
+                    <input type="e-mail" value={editUser.email} onChange={updateEditUserField('email')} className="input-txt" placeholder="E-mail"/>
+                    {/*<input type="password" value={editUser.password} onChange={updateEditUserField('password')} className="input-txt" placeholder="Пароль"/>*/}
+                    <input type="text" value={editUser.avatar} onChange={updateEditUserField('avatar')}  className="input-txt" placeholder="Url фото"/>
+                    <button onClick={updateUser} className="btn">Сохранить</button>
+                </div>
+            </div>
+        }
         <div className="admin-container">
 
 
@@ -101,8 +148,8 @@ const AdminUsers = ({ isLoading, users, error }) => {
                                 <p>{ user.email }</p>
                             </div>
                             <div className="col">
-                                <img src="/img/ban.svg" alt className="edit__user"/>
-                                <img src="/img/edit.svg" alt className="edit__user"/>
+                                <img src="/img/ban.svg"  alt className="edit__user"/>
+                                <img src="/img/edit.svg" onClick={() => openEditUserModal(user)} alt className="edit__user"/>
                                 <button className="btn" onClick={ () => {
                                     let message = prompt('Введите сообщение')
                                     connection.emit('SEND_MESSAGE', {
